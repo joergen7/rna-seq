6e4bc3ab74bcf4fd079a973aa6cdd8ad16028608
Jorgen Brandt <joergen.brandt@onlinehome.de>
2015-10-16 18:54:43 +0200

    Clean project.

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'rna-seq'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures rna-seq'
-long_description 'Installs/Configures rna-seq'
-version '0.1.0'
