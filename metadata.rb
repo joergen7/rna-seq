0b7122466cfd18287af79bb9fadde62c1073061d
Jorgen Brandt <joergen.brandt@onlinehome.de>
2015-10-16 20:23:17 +0200

    Workflow added. Dependencies corrected.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name 'rna-seq'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer "Jörgen Brandt"
+maintainer_email "brandjoe@hu-berlin.de"
 license 'all_rights'
 description 'Installs/Configures rna-seq'
 long_description 'Installs/Configures rna-seq'
 version '0.1.0'
+
+depends "chef-bioinf-worker"
+depends "chef-cuneiform"
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,9 @@
 # Cookbook Name:: rna-seq
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
+
+include_recipe "tophat"
+include_recipe "cufflinks"
+include_recipe "cummerbund"
+include_recipe "chef-cuneiform::default"
