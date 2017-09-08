cbbeac8a19227b956143cfc0833611c974d74ac3
Jörgen <joergen.brandt@onlinehome.de>
2017-09-08 15:03:45 +0200

    RNA-Seq workflow fixed.

--- recipes/data.rb
+++ recipes/data.rb
@@ -6,15 +6,15 @@
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
 fq_link = "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE32nnn/GSE32038/suppl/GSE32038_simulated_fastq_files.tar.gz"
-fq_dir  = "#{node.dir.data}/fastq"
+fq_dir  = "#{node["dir"]["data"]}/fastq"
 fq_tar  = "#{fq_dir}/#{File.basename( fq_link )}"
 
 bdgp6_link = "ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Drosophila_melanogaster/Ensembl/BDGP6/Drosophila_melanogaster_Ensembl_BDGP6.tar.gz"
-bdgp6_tar = "#{node.dir.archive}/#{File.basename( bdgp6_link )}"
-bdgp6_dir = "#{node.dir.data}/BDGP6"
+bdgp6_tar = "#{node["dir"]["archive"]}/#{File.basename( bdgp6_link )}"
+bdgp6_dir = "#{node["dir"]["data"]}/BDGP6"
 
-directory node.dir.data
-directory node.dir.archive
+directory node["dir"]["data"]
+directory node["dir"]["archive"]
 directory fq_dir
 
 # download FastQ data
@@ -44,9 +44,9 @@ end
 
 bash "extract_bdgp6" do
   code <<-SCRIPT
-tar xf #{bdgp6_tar} -C #{node.dir.data} Drosophila_melanogaster/Ensembl/BDGP6
-mv #{node.dir.data}/Drosophila_melanogaster/Ensembl/BDGP6 #{node.dir.data}/BDGP6
-rm -rf #{node.dir.data}/Drosophila_melanogaster
+tar xf #{bdgp6_tar} -C #{node["dir"]["data"]} Drosophila_melanogaster/Ensembl/BDGP6
+mv #{node["dir"]["data"]}/Drosophila_melanogaster/Ensembl/BDGP6 #{node["dir"]["data"]}/BDGP6
+rm -rf #{node["dir"]["data"]}/Drosophila_melanogaster
   SCRIPT
   not_if "#{Dir.exists?( bdgp6_dir )}"
 end
--- recipes/workflow.rb
+++ recipes/workflow.rb
@@ -5,10 +5,10 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-directory node.dir.wf
+directory node["dir"]["wf"]
 
 # place workflow
-template "#{node.dir.wf}/rna-seq.cf" do
+template "#{node["dir"]["wf"]}/rna-seq.cf" do
   source "rna-seq.cf.erb"
 end
 
